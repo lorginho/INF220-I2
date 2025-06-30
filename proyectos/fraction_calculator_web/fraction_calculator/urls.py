df6bdcfccef8dd7b0285478ac9934b81c197d80d
lorginho <lorgioanez@gmail.com>
2025-06-30 19:59:17 -0400

    Eliminar carpetas fraction_calculator del repositorio

--- proyectos/fraction_calculator_web/fraction_calculator/urls.py
+++
@@ -1,8 +0,0 @@
-# ============= ARCHIVO: fraction_calculator/urls.py =============
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('src.urls')),
-]
--- proyectos/fraction_calculator_web/src/apps.py
+++
@@ -1,6 +0,0 @@
-# ============= ARCHIVO: src/apps.py =============
-from django.apps import AppConfig
-
-class FractionsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'src'
